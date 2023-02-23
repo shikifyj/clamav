a84087edd3f70db36c2277cce7811cf3a7c41bbf
shiki <237303802@qq.com>
2023-02-23 16:55:19 +0800

    测试

--- main.py
+++ main.py
@@ -21,23 +21,26 @@ class InputParser(object):
                                  dest='version',
                                  help='Show current version',
                                  action='store_true')
-        self.parser_filepath = subp.add_parser("filepath", aliases=['f'], help='Directory to scan',
-                                               )
-        self.parser_filepath.add_argument(dest='filepath', help='')
-        self.parser_volumes = subp.add_parser("volumes", aliases=['p'], help='Pvc to scan')
-        self.parser_volumes.add_argument(dest='volumes', help='')
-
-        self.parser_filepath.set_defaults(func=self.filepath_func)
-        self.parser_volumes.set_defaults(func=self.volumes_func)
+        self.scan = subp.add_parser("virus_scan", aliases=['v'], help="Scan virus")
+        self.scan.add_argument('-f',
+                               '--filepath',
+                               dest='filepath',
+                               help='Directory to scan',
+                               action='store')
+        self.scan.add_argument('-p',
+                               '--volumes',
+                               dest='volumes',
+                               help='Pvc to scan',
+                               action='store')
+        self.scan.set_defaults(func=self.scan_func)
         self.parser.set_defaults(func=self.help_usage)
 
-    def filepath_func(self, args):
-        control.AntiVirus(filepath=args.filepath)
-        logger.write_to_log("INFO", f"Start creating Pod for {args.filepath}")
+    def scan_func(self, args):
+        if args.filepath:
+            control.AntiVirus(filepath=args.filepath)
+        elif args.volumes:
+            control.AntiVirus(claimname=args.volumes)
 
-    def volumes_func(self, args):
-        control.AntiVirus(claimname=args.volumes)
-        logger.write_to_log("INFO", f"Start creating Pod for {args.volumes}")
     def help_usage(self, args):
         if args.version:
             print(f'Version: {consts.VERSION}')
